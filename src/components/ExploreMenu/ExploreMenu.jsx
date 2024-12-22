eb5a6fc860b035759f0247b82c91b4cea1dbad78
NofelShahbaz <nofelshahbaz420@gmail.com>
2024-12-22 18:47:24 +0500

    Update ExploreMenu.jsx

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -7,7 +7,7 @@ const ExploreMenu = ({category,setCategory}) => {
   return (
     <div className='explore-menu' id='explore-menu'>
         <h1>Explore our Menu</h1>
-        <p className='explore-menu-text'>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary
+        <p className='explore-menu-text'>Choose from a diverse menu of your liking featuring a delectable array of dishes crafted with the finest ingredients and culinary
         expertise. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
       <div className="explore-menu-list">
         {menu_list.map((item,index)=>{
