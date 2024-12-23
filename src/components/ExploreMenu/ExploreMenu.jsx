ab9973b153b909f322780663fc9e8b62c09c9bad
Muhammad Aleem <maleem9564@gmail.com>
2024-12-23 22:25:23 +0500

    Update ExploreMenu.jsx

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -7,13 +7,8 @@ const ExploreMenu = ({category,setCategory}) => {
   return (
     <div className='explore-menu' id='explore-menu'>
         <h1>Explore our Menu</h1>
-<<<<<<< HEAD
         <p className='explore-menu-text'>Choose from a diverse menu featuring a delectable and delicious array of dishes crafted with the finest ingredients and culinary
         expertise. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
-=======
-        <p className='explore-menu-text'>Choose from a diverse menu of your liking featuring a delectable array of dishes crafted with the finest ingredients and culinary
-        expertise. Our primary mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
->>>>>>> 13bda7e113a6bad5a2791af5e1426b3c5903fc68
       <div className="explore-menu-list">
         {menu_list.map((item,index)=>{
             return(
