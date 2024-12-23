331e5086b0f9651695829fdf5f5672ad2a34b18f
NofelShahbaz <nofelshahbaz420@gmail.com>
2024-12-23 22:57:07 +0500

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
         <div className="header-contents">
             <h2>Order your favourite food here</h2>
             <p>Choose from a diverse menu featuring a delectable variety of dishes crafted with the finest ingridients and culinary
-               expertise. Our main priority is to satiate your cravings and hoist your dining experience, one delicious meal at a time.
+               expertise. Our major priority is to satiate your cravings and hoist your dining experience, one delicious meal at a time.
             </p>
             <button>View Menu</button>
         </div>
