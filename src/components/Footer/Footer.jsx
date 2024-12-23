427efbd20ca63ef7e0e3ec968634b931f7fd48c2
NofelShahbaz <nofelshahbaz420@gmail.com>
2024-12-23 21:10:08 +0500

    updated weak grammer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
             <div className="footer-content-left">
                <img src={assets.logo} alt='' />
                <p>Tomato is a vibrant restaurant commemorating the versatility of tomatoes. Located in the heart of the city, we offer 
-                a menu brimming with fresh, locally sourced tomato-based dishes—from savory soups and salads to creative small plates 
+                a menu overflowing with fresh, locally sourced tomato-based dishes—from savory soups and salads to creative small plates 
                 and desserts. Enjoy a warm, inviting atmosphere and a nostalgic dining experience with every bite</p>
                <div className="footer-social-icons">
                   <img src={assets.facebook_icon} alt='' />
