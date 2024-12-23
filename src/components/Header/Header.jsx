b469ff1eac302e2b357582d0b50cb0d8354e4964
NofelShahbaz <nofelshahbaz420@gmail.com>
2024-12-24 00:04:02 +0500

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     <div className='header'>
         <div className="header-contents">
             <h2>Order your favourite food here</h2>
-            <p>Choose from a diverse menu featuring a delectable variety of dishes crafted with the finest ingridients and culinary
+            <p>Choose from a diverse menu featuring a delectable variety of dishes crafted with the finest ingredients and culinary
                expertise. Our major priority is to satiate your cravings and hoist your dining experience, one delicious meal at a time.
             </p>
             <button>View Menu</button>
